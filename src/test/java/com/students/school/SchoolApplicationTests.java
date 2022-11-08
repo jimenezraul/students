3288c2a9169b40b4df70c25e9d72ed1b3dd9e0d9
Raul <jimenezraul1981@gmail.com>
2022-11-08 17:37:27 -0500

    initial commit

---
+++ src/test/java/com/students/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.students.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
